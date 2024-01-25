f5517305d7d6c88c96b61b25b27e916782f6f33b
Hanun Quran <hanunhfzhhn@gmail.com>
2024-01-25 09:47:38 +0700

    This app is linked to the debug service: ws://127.0.0.1:54792/QxfNAGIBgx8=/ws Debug service listening on ws://127.0.0.1:54792/QxfNAGIBgx8=/ws

---
+++ lib/UI/home_screen.dart
@@ -0,0 +1,8 @@
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const scaffold();
+  }
+}
---
+++ lib/global.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF672CBC); //-> 0xFF untuk mengintegrasi warna integer ke string
+Color secondary = const Color(0xFF8789a3);
+Color button = const Color(0xFFF9B091);
