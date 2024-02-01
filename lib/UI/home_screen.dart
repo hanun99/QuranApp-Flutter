4547324b9146e3b9337a61aadb3abe63c96cccf6
Hanun Quran <hanunhfzhhn@gmail.com>
2024-02-01 07:58:36 +0700

    This app is linked to the debug service: ws://127.0.0.1:54792/QxfNAGIBgx8=/ws Debug service listening on ws://127.0.0.1:54792/QxfNAGIBgx8=/ws

--- lib/UI/home_screen.dart
+++ lib/UI/home_screen.dart
@@ -1,8 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quranapp/UI/home_screen.dart';
+import 'package:quranapp/global.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const scaffold();
+    return Scaffold(
+      appBar: _appBar(),
+      bottomNavigationBar: _bottomNavigationBar(),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: _salam()
+    ),
+    );
   }
+
+  Column _salam() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+      Text(
+      'Assalamualaikum',
+      style: GoogleFonts.poppins(
+        color : secondary,
+        fontWeight: FontWeight.w600,
+        fontSize: 18,
+      ),
+    ),
+    const SizedBox(
+      height: 4,
+    ),
+    Text(
+      'Hanun Hafizhoh',
+      style: GoogleFonts.poppins(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      ),
+    ),
+        const SizedBox(
+          height: 24,
+        ),
+        _lastRead()
+    ],
+    );
+  }
+
+  Stack _lastRead() {
+    return Stack(children: [
+        Container(
+          height: 131,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFDF98FA),
+                Color(0xFF9055FF)
+              ])),
+
+    ),
+      Positioned(
+        bottom: 0,
+        right: 0,
+        child: SvgPicture.asset('/svg/banner_quran.svg')
+      ),
+      Column(
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset('/svg/last_read.svg'),
+              const SizedBox(width: 8,),
+          Text(
+              'Last Read',
+               style: GoogleFonts.poppins(
+                 color: Colors.white,
+                 fontSize: 14,
+                 fontWeight: FontWeight.w600
+               ),
+          )
+            ]
+          )
+        ],
+      )
+  ],
+      );
+  }
+
+  AppBar _appBar() => AppBar(
+    backgroundColor: Colors.white,
+    automaticallyImplyLeading: false,
+    title: Row(
+      children: [
+        IconButton(
+            onPressed: (() => {}),
+            icon: SvgPicture.asset('/svg/menu_icon.svg')
+        ),
+        SizedBox(
+          width: 24,
+        ),
+        Text(
+          'Quran App',
+          style: GoogleFonts.poppins(
+            color: primary,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        const Spacer(),
+        IconButton(
+            onPressed: (() => {}),
+            icon: SvgPicture.asset('/svg/search_icon.svg')
+        ),
+      ],
+    ),
+    actions: [
+    ],
+  );
+
+
+  BottomNavigationBar _bottomNavigationBar() => BottomNavigationBar(
+    items: [
+      _bottomNavigationBarItem(icon: '/svg/icon.svg', label: 'Quran'),
+      _bottomNavigationBarItem(icon: '/svg/doa.svg', label: 'Doa'),
+      _bottomNavigationBarItem(icon: '/svg/bookmark.svg', label: 'Bookmark'),
+
+    ],);
+
+  BottomNavigationBarItem _bottomNavigationBarItem({required String icon, required String label}) =>
+BottomNavigationBarItem(
+    icon: SvgPicture.asset(
+    icon,
+      color: secondary,
+    ),
+  activeIcon: SvgPicture.asset(
+    icon,
+    color: primary,
+  ),
+  label: label,
+    );
+
+
+
+
 }
--- lib/UI/splash_screen.dart
+++ lib/UI/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quranapp/global.dart';
+import 'package:quranapp/UI/home_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -10,10 +12,10 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16),
+        padding: const EdgeInsets.symmetric(vertical: 16), //-> EdgeInsets.symmetric -> for padding top and bottom
         child: Center(
           child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center, //-> untuk alignment / rata tengah untuk teksnya | -> main untuk
+            mainAxisAlignment: MainAxisAlignment.center, //-> untuk alignment / rata tengah untuk teksnya | -> main untuk
             crossAxisAlignment: CrossAxisAlignment.center,// cross untuk antar element
             children: [
               Text(
@@ -24,27 +26,36 @@ class SplashScreen extends StatelessWidget {
                   fontSize: 28,
                 ),
               ),
+              const SizedBox(
+                height: 16,
+              ),
               Text(
                 'Learn Quran and Recite once everyday',
                 style : GoogleFonts.poppins(
                 color : secondary,
                 fontSize : 18,
                 ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 50,
               ),
               Stack(
+                clipBehavior: Clip.none,
                 children: [
                 Container(
                   height: 450,
-                  width: 450,
+                  width: double.infinity,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     color: primary,
                   ),
-                  child: SvgPicture.asset('assets/svg/splash.svg'),
+                  child:
+                  SvgPicture.asset('svg/splash.svg'),
                 ),
                   Positioned(
                     left: 0,
-                    bottom: 0,
+                    bottom: -30,
                     right: 0,
                     child: Center(
                       child: GestureDetector(
@@ -55,8 +66,8 @@ class SplashScreen extends StatelessWidget {
                         },
                         child: Container(
                           padding: const EdgeInsets.symmetric(
-                            horizontal: 50,
-                            vertical: 50,
+                            horizontal: 40,
+                            vertical: 15,
                           ),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(30),
